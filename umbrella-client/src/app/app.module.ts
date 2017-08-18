f10c24418345236a634a6c1b03186e61f30e7374
Raphael Lima <rlima@gaveainvest.com.br>
2017-08-18 11:13:05 -0300

    Setup inicial do ng2-signalr

--- umbrella-client/src/app/app.module.ts
+++ umbrella-client/src/app/app.module.ts
@@ -3,12 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+// SignalR
+import { SignalRModule, SignalRConfiguration } from 'ng2-signalr';
+
+import { environment } from '../environments/environment';
+
+// v2.0.0
+export function createConfig(): SignalRConfiguration {
+  const c = new SignalRConfiguration();
+  c.hubName = environment.hubName;
+  c.withCredentials = true;
+  c.url = environment.signalrUrlBase;
+  c.logging = true;
+  return c;
+}
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    SignalRModule.forRoot(createConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
