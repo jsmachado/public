0fe23292dbf485c21e6f2f5b2050d641a1bdfa67
Raphael Lima <rlima@gaveainvest.com.br>
2017-08-18 12:17:22 -0300

    Implementando a notificação no app.component

--- umbrella-client/src/app/app.component.ts
+++ umbrella-client/src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from "rxjs/Rx";
+import { SignalR } from 'ng2-signalr';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+
+  ticker: string;
+
+  constructor(private _signalR: SignalR) { }
+
+  public ngOnInit(): void {
+    let clientReceiveMessage$: Observable<string> = Observable
+      .fromPromise(this._signalR.connect())
+      .flatMap(conn => conn.listenFor("clientReceiveMessage"));
+
+    clientReceiveMessage$.subscribe(message => {
+      this.ticker = message;
+    });
+  }
 }
